44a011e5c52d59376b41bd5eddaa20c3f314830d
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-03-23 00:33:35 +0900

    add sample app.

--- main.go
+++ main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/atsushi-kitazawa/google_spreadsheet_tui_tool/google_auth"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
 )
 
 func main() {
-    fmt.Println("Hello")
+        b, err := ioutil.ReadFile("credentials.json")
+        if err != nil {
+                log.Fatalf("Unable to read client secret file: %v", err)
+        }
+
+        // If modifying these scopes, delete your previously saved token.json.
+        config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
+        if err != nil {
+                log.Fatalf("Unable to parse client secret file to config: %v", err)
+        }
+	client := google_auth.GetClient(config)
+
+        srv, err := sheets.New(client)
+        if err != nil {
+                log.Fatalf("Unable to retrieve Sheets client: %v", err)
+        }
+
+        // Prints the names and majors of students in a sample spreadsheet:
+        // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+        spreadsheetId := "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
+        readRange := "Class Data!A2:E2"
+        resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+        if err != nil {
+                log.Fatalf("Unable to retrieve data from sheet: %v", err)
+        }
+
+        if len(resp.Values) == 0 {
+                fmt.Println("No data found.")
+        } else {
+                fmt.Println("Name, Major:")
+                for _, row := range resp.Values {
+                        // Print columns A and E, which correspond to indices 0 and 4.
+                        fmt.Printf("%s, %s\n", row[0], row[3])
+                }
+        }
 }
